1306dacf3c440d6ad53c56bca8197aa6e31ca74d
stevn4127 <stevn4127@gmail.com>
2022-11-06 03:11:05 -0500

    Add overlays

--- device-raven.mk
+++ device-raven.mk
@@ -260,3 +260,7 @@ endif
 # Device features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
+DEVICE_PACKAGE_OVERLAYS += \
+	device/google/raviole/overlay_common \
+	device/google/raviole/overlay_bliss
